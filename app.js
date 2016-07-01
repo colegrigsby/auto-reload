72f2c66611c6e5f4a1ef41ae2b2f4069de918736
colegrigsby <cole.grigsby@gmail.com>
2016-07-01 10:15:09 -0400

    reload signal

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
 var pmx = require('pmx');
-var pm2     = require('pm2');
+var pm2 = require('pm2');
 var Promise = require('bluebird');
 var vizion = require('vizion');
-var child                = require('child_process');
+var child = require('child_process');
 
 /******************************
  *    ______ _______ ______
@@ -39,159 +38,163 @@ var child                = require('child_process');
  */
 pmx.initModule({
 
-  // Options related to the display style on Keymetrics
-  widget : {
+    // Options related to the display style on Keymetrics
+    widget: {
 
-    // Logo displayed
-    logo : 'https://app.keymetrics.io/img/logo/keymetrics-300.png',
+        // Logo displayed
+        logo: 'https://app.keymetrics.io/img/logo/keymetrics-300.png',
 
-    // Module colors
-    // 0 = main element
-    // 1 = secondary
-    // 2 = main border
-    // 3 = secondary border
-    theme            : ['#141A1F', '#222222', '#3ff', '#3ff'],
+        // Module colors
+        // 0 = main element
+        // 1 = secondary
+        // 2 = main border
+        // 3 = secondary border
+        theme: ['#141A1F', '#222222', '#3ff', '#3ff'],
 
-    // Section to show / hide
-    el : {
-      probes  : true,
-      actions : true
-    },
+        // Section to show / hide
+        el: {
+            probes: true,
+            actions: true
+        },
 
-    // Main block to show / hide
-    block : {
-      actions : false,
-      issues  : true,
-      meta    : true,
+        // Main block to show / hide
+        block: {
+            actions: false,
+            issues: true,
+            meta: true,
 
-      // Custom metrics to put in BIG
-      main_probes : ['test-probe']
+            // Custom metrics to put in BIG
+            main_probes: ['test-probe']
+        }
+
     }
 
-  }
+}, function (err, conf) {
 
-}, function(err, conf) {
-
-  /**
-   * Module specifics like connecting to a database and
-   * displaying some metrics
-   */
-
-  /**
-   *                      Custom Metrics
-   *
-   * Let's expose some metrics that will be displayed into Keymetrics
-   *   For more documentation about metrics: http://bit.ly/1PZrMFB
-   */
-  var Probe = pmx.probe();
-
-  var value_to_inspect = 0;
-
-  /**
-   * .metric, .counter, .meter, .histogram are also available (cf doc)
-   */
-  var val = Probe.metric({
-    name : 'test-probe',
-    value : function() {
-      return value_to_inspect;
-    },
     /**
-     * Here we set a default value threshold, to receive a notification
-     * These options can be overriden via Keymetrics or via pm2
-     * More: http://bit.ly/1O02aap
+     * Module specifics like connecting to a database and
+     * displaying some metrics
      */
-    alert : {
-      mode     : 'threshold',
-      value    : 20,
-      msg      : 'test-probe alert!',
-      action   : function(val) {
-        // Besides the automatic alert sent via Keymetrics
-        // You can also configure your own logic to do something
-        console.log('Value has reached %d', val);
-      }
-    }
-  });
 
-  setInterval(function() {
+    /**
+     *                      Custom Metrics
+     *
+     * Let's expose some metrics that will be displayed into Keymetrics
+     *   For more documentation about metrics: http://bit.ly/1PZrMFB
+     */
+    var Probe = pmx.probe();
 
-    var chain = Promise.resolve();
-    var running = false;
+    var value_to_inspect = 0;
 
-
-    if (running == true) return false;
-
-    running = true; //nec with chaining?? //probably gonna device between running vs promises?
-    // Then we can see that this value increase over the time in Keymetrics
-    //PROMISE CHAIN???? for pull and restart or something based on give proc names
-      vizion.update(
-          {folder : "/opt/dev/source"}, //TODO from conf file
-          function(err, meta){
-            console.log("meta", meta);
-            console.log("err", err);
-            //TODO exec start or restart?
-            console.log(process)
-            //child.exec("Echo hello ", process);
-            running = false;
-          }
-      );
-
-
-    value_to_inspect++;
-  }, 3000);
-
-  console.log(value_to_inspect);
-
-  /**
-   *                Simple Actions
-   *
-   *   Now let's expose some triggerable functions
-   *  Once created you can trigger this from Keymetrics
-   *
-   */
-  pmx.action('env', function(reply) {
-    return reply({
-      env: process.env
-    });
-  });
-
-  /**
-   *                 Scoped Actions
-   *
-   *     This are for long running remote function
-   * This allow also to res.emit logs to see the progress
-   *
-   **/
-  var spawn = require('child_process').spawn;
-
-  pmx.scopedAction('lsof cmd', function(options, res) {
-    var child = spawn('lsof', []);
-
-    child.stdout.on('data', function(chunk) {
-      chunk.toString().split('\n').forEach(function(line) {
+    /**
+     * .metric, .counter, .meter, .histogram are also available (cf doc)
+     */
+    var val = Probe.metric({
+        name: 'test-probe',
+        value: function () {
+            return value_to_inspect;
+        },
         /**
-         * Here we send logs attached to this command
+         * Here we set a default value threshold, to receive a notification
+         * These options can be overriden via Keymetrics or via pm2
+         * More: http://bit.ly/1O02aap
          */
-        res.send(line);
-      });
+        alert: {
+            mode: 'threshold',
+            value: 20,
+            msg: 'test-probe alert!',
+            action: function (val) {
+                // Besides the automatic alert sent via Keymetrics
+                // You can also configure your own logic to do something
+                console.log('Value has reached %d', val);
+            }
+        }
     });
 
-    child.stdout.on('end', function(chunk) {
-      /**
-       * Then we emit end to finalize the function
-       */
-      res.end('end');
+
+    console.log(value_to_inspect);
+
+    /**
+     *                Simple Actions
+     *
+     *   Now let's expose some triggerable functions
+     *  Once created you can trigger this from Keymetrics
+     *
+     */
+    pmx.action('env', function (reply) {
+        return reply({
+            env: process.env
+        });
     });
 
-  });
+    /**
+     *                 Scoped Actions
+     *
+     *     This are for long running remote function
+     * This allow also to res.emit logs to see the progress
+     *
+     **/
+    var spawn = require('child_process').spawn;
 
-/*
-  pm2.connect(function(){
-    console.log('auto-reload2 module connected to pm2');
+    pmx.scopedAction('lsof cmd', function (options, res) {
+        var child = spawn('lsof', []);
+
+        child.stdout.on('data', function (chunk) {
+            chunk.toString().split('\n').forEach(function (line) {
+                /**
+                 * Here we send logs attached to this command
+                 */
+                res.send(line);
+            });
+        });
+
+        child.stdout.on('end', function (chunk) {
+            /**
+             * Then we emit end to finalize the function
+             */
+            res.end('end');
+        });
+
+    });
 
 
-  })
+    pm2.connect(function () {
+        console.log('auto-reload2 module connected to pm2');
+        setInterval(function () {
 
-*/
+            var chain = Promise.resolve();
+            var running = false;
+
+
+            if (running == true) return false;
+
+            running = true; //nec with chaining?? //probably gonna device between running vs promises?
+            // Then we can see that this value increase over the time in Keymetrics
+            //PROMISE CHAIN???? for pull and restart or something based on give proc names
+            vizion.update(
+                {folder: "/opt/dev/source"}, //TODO from conf file
+                function (err, meta) {
+                    console.log("meta", meta);
+                    console.log("err", err);
+                    //TODO exec start or restart?
+                    console.log(process) //this is the current reload process
+                    //child.exec("Echo hello ", process);
+
+                    pm2.sendSignalToProcessName("reload", "asahi");
+
+
+                    running = false;
+                }
+            );
+
+
+            //idea - get process id by name, then send signal to the process id (restart)
+
+
+            value_to_inspect++;
+        }, 3000);
+
+    })
 
 
 });
