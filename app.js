81b296d3f867c07c7ae3b493cc0c8d55db5fc9f8
colegrigsby <cole.grigsby@gmail.com>
2016-07-05 11:49:23 -0400

    cb

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ var conf = pmx.initModule({
                 //TODO exec start or restart?
                 //console.log(process) //this is the current reload process
                 //child.exec("Echo hello ", process);
-                console.log("ID:", pm2.getProcessIdByName("asahi", function(blank, id){return id}))
+                pm2.getProcessIdByName("asahi", function(blank, id){console.log("ID:",id)})
 
 
                 //might have to chain? definitely a good idea
